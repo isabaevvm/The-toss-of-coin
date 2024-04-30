d822d0043c7d9386dc9b9272e7c648d687391afa
Isabaev Mukhammed <isabaevvm@gmail.com>
2024-04-30 09:58:38 +0600

    git initial commit

---
+++ src/main/java/com/example/Thetossofcoin/TheTossOfCoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.Thetossofcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheTossOfCoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheTossOfCoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Thetossofcoin/TheTossOfCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Thetossofcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheTossOfCoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
